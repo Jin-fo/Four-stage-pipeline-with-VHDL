fdc08126f78cafa72d3f37ec9d1594e6245ce6d5
Jin <134983017+Jin-fo@users.noreply.github.com>
2026-03-07 19:58:02 -0500

    Add files via upload

---
+++ src/s2_decode/if_id.vhd
@@ -0,0 +1,45 @@
+library IEEE;
+use IEEE.std_logic_1164.all; 
+use work.numeric_var.all;
+
+entity if_id is
+	port(				  
+		clk			: in std_logic;	  
+		enable		: in std_logic;
+		reset_bar 	: in std_logic;		
+		flush_ctrl	: in std_logic;
+		
+		if_pc		: in std_logic_vector(COUNTER_LENGTH-1 downto 0);
+		if_instruc 	: in std_logic_vector(INSTRUCTION_LENGTH-1 downto 0);  
+		iff_target	: in std_logic_vector(COUNTER_LENGTH-1 downto 0);
+		
+		id_pc		: out std_logic_vector(COUNTER_LENGTH-1 downto 0);
+		id_instruc 	: out std_logic_vector(INSTRUCTION_LENGTH-1 downto 0):= (others => '-'); 
+		ifd_target	: out std_logic_vector(COUNTER_LENGTH-1 downto 0)
+	);
+end if_id;
+
+
+architecture behavior of if_id is
+begin		
+    
+    -- ? BETTER: Proper synchronous process
+    if_id : process (clk, reset_bar)	
+    begin 
+        if reset_bar = '0' then
+            id_instruc  <= NOP_INSTRUCTION;
+            id_pc       <= (others => '-');
+            ifd_target  <= (others => '-');
+        elsif rising_edge(clk) then	
+            if flush_ctrl = '1' then
+                id_instruc  <= NOP_INSTRUCTION;
+                id_pc       <= (others => '-');
+                ifd_target  <= (others => '-');
+            elsif enable = '1' then
+                id_instruc  <= if_instruc;
+                id_pc       <= if_pc;
+                ifd_target  <= iff_target;
+            end if;   
+        end if;	 		
+    end process;
+end architecture;
---
+++ src/s2_decode/register_file.vhd
@@ -0,0 +1,89 @@
+library ieee;
+use ieee.std_logic_1164.all; 
+use ieee.numeric_std.all;
+use work.numeric_var.all;
+
+entity register_file is
+	port(		   
+	read_sel	: in std_logic_vector(2 downto 0);
+	
+	id_rs3_ptr	: in std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+	id_rs2_ptr	: in std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+	id_rs1_ptr  : in std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+	
+	wb_rd		: in std_logic_vector(REGISTER_LENGTH-1 downto 0);
+	wb_rd_ptr	: in std_logic_vector(ADDRESS_LENGTH-1 downto 0);  --for forwarding address comparision
+	wb_wback	: in std_logic;	
+	
+	out_file	: out std_logic_vector(REGISTER_SIZE-1 downto 0);
+	
+	id_rs3		: out std_logic_vector(REGISTER_LENGTH-1 downto 0);
+	id_rs2		: out std_logic_vector(REGISTER_LENGTH-1 downto 0);
+	id_rs1		: out std_logic_vector(REGISTER_LENGTH-1 downto 0)	 
+	);
+end entity;
+
+architecture behavior of register_file is  
+	signal REG_FILE	: std_logic_vector(REGISTER_SIZE-1 downto 0) := (others => '0');
+begin		
+		
+	register_file : process(read_sel, id_rs3_ptr, id_rs2_ptr, id_rs1_ptr, wb_wback)		
+		variable var_rs3_ptr	: std_logic_vector(ADDRESS_LENGTH-1 downto 0) := (others => '-');
+		variable var_rs2_ptr	: std_logic_vector(ADDRESS_LENGTH-1 downto 0) := (others => '-');
+		variable var_rs1_ptr	: std_logic_vector(ADDRESS_LENGTH-1 downto 0) := (others => '-');
+	
+		variable var_rs3 	: std_logic_vector(REGISTER_LENGTH-1 downto 0);
+		variable var_rs2 	: std_logic_vector(REGISTER_LENGTH-1 downto 0);
+		variable var_rs1 	: std_logic_vector(REGISTER_LENGTH-1 downto 0);
+		variable read_select	: std_logic_vector(2 downto 0) := (others => '0');
+	begin			   
+	    -----------------------------------------------------------------
+        -- Read register values
+        -----------------------------------------------------------------
+		if read_sel(2) = '1' then
+		var_rs3 := REG_FILE(
+			(to_integer(unsigned(var_rs3_ptr))+1)*(REGISTER_LENGTH)-1
+			downto 
+			to_integer(unsigned(var_rs3_ptr))*(REGISTER_LENGTH)); 
+		end if;
+		if read_sel(1) = '1' then
+		var_rs2 := REG_FILE(
+			(to_integer(unsigned(var_rs2_ptr))+1)*(REGISTER_LENGTH)-1 
+			downto 
+			to_integer(unsigned(var_rs2_ptr))*(REGISTER_LENGTH));
+		end if;
+		if read_sel(0) = '1' then
+		var_rs1 := REG_FILE(
+			(to_integer(unsigned(var_rs1_ptr))+1)*(REGISTER_LENGTH)-1 
+			downto 
+			to_integer(unsigned(var_rs1_ptr))*(REGISTER_LENGTH)); 
+		end if;		  
+		
+        -------------------------------------------------------------------
+        -- FORWARDING
+        -------------------------------------------------------------------
+        if wb_wback = '1' then
+            if wb_rd_ptr = var_rs3_ptr then
+                var_rs3 := wb_rd;
+            end if;
+            if wb_rd_ptr = var_rs2_ptr then
+                var_rs2 := wb_rd;
+            end if;
+            if wb_rd_ptr = var_rs1_ptr then
+                var_rs1 := wb_rd;
+            end if;	
+			REG_FILE(
+                (to_integer(unsigned(wb_rd_ptr))+1)*REGISTER_LENGTH - 1 downto
+				to_integer(unsigned(wb_rd_ptr))*REGISTER_LENGTH) <= wb_rd;	
+        end if;		
+		
+        -----------------------------------------------------------------
+        -- Drive outputs
+        -----------------------------------------------------------------
+		id_rs3 <= var_rs3;
+		id_rs2 <= var_rs2;
+		id_rs1 <= var_rs1;
+		
+		out_file <= REG_FILE;
+	end process;   
+end architecture;
