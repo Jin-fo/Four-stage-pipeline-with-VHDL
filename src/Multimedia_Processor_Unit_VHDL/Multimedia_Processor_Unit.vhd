71f0180d04427a752a808e74220f82985171ae9d
Jin <134983017+Jin-fo@users.noreply.github.com>
2026-03-07 21:20:40 -0500

    Add files via upload

---
+++ src/Multimedia_Processor_Unit_VHDL/Multimedia_Processor_Unit.vhd
@@ -0,0 +1,265 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.numeric_var.all;
+use work.all;
+
+entity Multimedia_Processor_Unit is	 
+	port (		 	  
+	--unit input
+	clk 		: in std_logic;	  
+	enable		: in std_logic;
+	reset_bar 	: in std_logic;
+	in_file 	: in std_logic_vector(FILE_SIZE-1 downto 0);   
+	
+	--unit output
+	out_file_tb	: out std_logic_vector(REGISTER_SIZE-1 downto 0);  
+	
+	--test beanch signals
+    pc_count_tb    : out std_logic_vector(COUNTER_LENGTH-1 downto 0);
+    if_instruc_tb  : out std_logic_vector(INSTRUCTION_LENGTH-1 downto 0);
+    id_instruc_tb  : out std_logic_vector(INSTRUCTION_LENGTH-1 downto 0);
+
+    id_opcode_tb   : out std_logic_vector(OPCODE_LENGTH-1 downto 0);
+    ex_opcode_tb   : out std_logic_vector(OPCODE_LENGTH-1 downto 0);
+
+    id_rs3_tb      : out std_logic_vector(REGISTER_LENGTH-1 downto 0);
+    id_rs2_tb      : out std_logic_vector(REGISTER_LENGTH-1 downto 0);
+    id_rs1_tb      : out std_logic_vector(REGISTER_LENGTH-1 downto 0);
+
+    ex_rs3_tb      : out std_logic_vector(REGISTER_LENGTH-1 downto 0);
+    ex_rs2_tb      : out std_logic_vector(REGISTER_LENGTH-1 downto 0);
+    ex_rs1_tb      : out std_logic_vector(REGISTER_LENGTH-1 downto 0);
+    ex_rd_tb       : out std_logic_vector(REGISTER_LENGTH-1 downto 0);
+
+    id_immed_tb    : out std_logic_vector(IMMEDIATE_LENGTH-1 downto 0);
+    ex_immed_tb    : out std_logic_vector(IMMEDIATE_LENGTH-1 downto 0);
+
+    id_rs3_ptr_tb  : out std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+    id_rs2_ptr_tb  : out std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+    id_rs1_ptr_tb  : out std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+    id_rd_ptr_tb   : out std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+
+    id_wback_tb    : out std_logic;
+
+    ex_rs3_ptr_tb  : out std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+    ex_rs2_ptr_tb  : out std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+    ex_rs1_ptr_tb  : out std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+    ex_rd_ptr_tb   : out std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+
+    ex_wback_tb    : out std_logic;
+
+    wb_rd_tb       : out std_logic_vector(REGISTER_LENGTH-1 downto 0);
+    wb_rd_ptr_tb   : out std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+    wb_wback_tb    : out std_logic
+	);
+end Multimedia_Processor_Unit;
+
+architecture structural of Multimedia_Processor_Unit is	 
+	signal out_file		: std_logic_vector(REGISTER_SIZE-1 downto 0);
+	signal pc_count 	: std_logic_vector(COUNTER_LENGTH-1 downto 0);
+	signal if_instruc 	: std_logic_vector(INSTRUCTION_LENGTH-1 downto 0);
+	signal id_instruc 	: std_logic_vector(INSTRUCTION_LENGTH-1 downto 0);	
+	
+	signal id_opcode 	  	: std_logic_vector(OPCODE_LENGTH-1 downto 0);	 
+	signal ex_opcode 	  	: std_logic_vector(OPCODE_LENGTH-1 downto 0);	
+	
+	signal id_rs3			: std_logic_vector(REGISTER_LENGTH-1 downto 0);
+	signal id_rs2			: std_logic_vector(REGISTER_LENGTH-1 downto 0);
+	signal id_rs1			: std_logic_vector(REGISTER_LENGTH-1 downto 0);	
+	
+	signal ex_rs3			: std_logic_vector(REGISTER_LENGTH-1 downto 0);
+	signal ex_rs2			: std_logic_vector(REGISTER_LENGTH-1 downto 0);
+	signal ex_rs1			: std_logic_vector(REGISTER_LENGTH-1 downto 0);	
+	signal ex_rd			: std_logic_vector(REGISTER_LENGTH-1 downto 0);
+	
+	signal id_immed		: std_logic_vector(IMMEDIATE_LENGTH-1 downto 0);
+	signal ex_immed		: std_logic_vector(IMMEDIATE_LENGTH-1 downto 0);
+	
+	signal id_rs3_ptr		: std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+	signal id_rs2_ptr		: std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+	signal id_rs1_ptr		: std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+	signal id_rd_ptr		: std_logic_vector(ADDRESS_LENGTH-1 downto 0); 
+	
+	signal id_wback		: std_logic;
+	
+	signal ex_rs3_ptr		: std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+	signal ex_rs2_ptr		: std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+	signal ex_rs1_ptr		: std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+	signal ex_rd_ptr		: std_logic_vector(ADDRESS_LENGTH-1 downto 0);
+	
+	signal ex_wback		: std_logic;
+	
+	signal wb_rd		: std_logic_vector(REGISTER_LENGTH-1 downto 0);
+	signal wb_rd_ptr	: std_logic_vector(ADDRESS_LENGTH-1 downto 0);		 
+	signal wb_wback		: std_logic;
+begin				 
+	P_C : entity work.pc(behavior)				  
+		port map (	 
+		--control
+		clk 		=> clk,	
+		enable		=> enable,
+		reset_bar 	=> reset_bar,
+		--output
+		pc_count 	=> pc_count); 
+
+	I_File : entity work.instruction_file(behavior)		
+		port map (	  
+		--input
+		pc_count 	=> pc_count,
+		in_file		=> in_file,	
+		reload_bar	=> reset_bar,
+		--output
+		instruc 	=> if_instruc);
+		
+	IF_ID : entity work.if_id(behavior)
+		port map (	
+		--control
+		clk			=> clk,	  
+		enable		=> enable,
+		reset_bar 	=> reset_bar,
+		--input
+		if_instruc 	=> if_instruc, 
+		--output
+		id_instruc  => id_instruc);
+		
+	R_File : entity work.register_file(behavior)
+		port map (
+		--input	  
+		clk 		=> clk,
+		instruc 	=> id_instruc, 
+		
+		--write back 
+		wb_rd		=> wb_rd,	  
+		wb_rd_ptr	=> wb_rd_ptr,
+		in_wback	=> wb_wback,	  
+		
+		--output			
+		out_file 	=> out_file,
+		opcode 		=> id_opcode,
+		
+		in_rs3	 	=> id_rs3,
+		in_rs2		=> id_rs2,
+		in_rs1		=> id_rs1,	 
+		
+		in_immed	=> id_immed,
+		
+		rs3_ptr 	=> id_rs3_ptr, 
+		rs2_ptr		=> id_rs2_ptr, 
+		rs1_ptr	   	=> id_rs1_ptr,
+		rd_ptr		=> id_rd_ptr,
+		out_wback	=> id_wback);
+		
+	ID_EX : entity work.id_ex(behavior)
+		port map (	
+		--control
+		clk 		=> clk,	 
+		enable		=> enable,
+		reset_bar 	=> reset_bar,
+		
+		--input
+		id_opcode	=> id_opcode,
+		
+		id_rs3		=> id_rs3,
+		id_rs2		=> id_rs2,
+		id_rs1 		=> id_rs1,
+		id_immed 	=> id_immed,
+		
+		id_rs3_ptr	=> id_rs3_ptr,			
+		id_rs2_ptr	=> id_rs2_ptr,		  
+		id_rs1_ptr	=> id_rs1_ptr, 
+		id_rd_ptr	=> id_rd_ptr, 
+		
+		id_wback	=> id_wback,
+		
+		--output
+		ex_opcode	=> ex_opcode,
+		
+		ex_rs3		=> ex_rs3,
+		ex_rs2		=> ex_rs2,
+		ex_rs1		=> ex_rs1,
+		ex_immed   	=> ex_immed,
+		
+		ex_rs3_ptr  => ex_rs3_ptr,
+		ex_rs2_ptr	=> ex_rs2_ptr,
+		ex_rs1_ptr	=> ex_rs1_ptr,
+		ex_rd_ptr	=> ex_rd_ptr,
+		
+		ex_wback	=> ex_wback); 
+		
+	MMU_ALU : entity work.mmu(behavior)
+		port map ( 
+		--inputs
+		opcode		=> ex_opcode,		
+		
+		in_rs3 		=> ex_rs3,
+		in_rs2		=> ex_rs2,
+		in_rs1		=> ex_rs1,
+		in_immed	=> ex_immed,
+		
+		rs3_ptr		=> ex_rs3_ptr,
+		rs2_ptr		=> ex_rs2_ptr,
+		rs1_ptr		=> ex_rs1_ptr,
+		--write back
+		wb_rd		=> wb_rd,
+		wb_rd_ptr	=> wb_rd_ptr,
+		in_wback	=> wb_wback,
+		
+		--output
+		out_rd		=> ex_rd);	
+		
+	EX_ID : entity work.ex_wb(behavior) 
+		port map ( 	 
+		--control
+		clk 		=> clk,
+		enable		=> enable,
+		reset_bar 	=> reset_bar,  
+		--input 
+		ex_rd		=> ex_rd,
+		ex_rd_ptr	=> ex_rd_ptr, 
+		ex_wback	=> ex_wback,
+		
+		--output 		
+		wb_rd		=> wb_rd,
+		wb_rd_ptr	=> wb_rd_ptr,
+		wback		=> wb_wback);		
+		
+	--exposed signal for test-beanch	  
+	out_file_tb <= out_file;
+	pc_count_tb    <= pc_count;
+	if_instruc_tb  <= if_instruc;
+	id_instruc_tb  <= id_instruc;
+	
+	id_opcode_tb   <= id_opcode;
+	ex_opcode_tb   <= ex_opcode;
+	
+	id_rs3_tb      <= id_rs3;
+	id_rs2_tb      <= id_rs2;
+	id_rs1_tb      <= id_rs1;
+	
+	ex_rs3_tb      <= ex_rs3;
+	ex_rs2_tb      <= ex_rs2;
+	ex_rs1_tb      <= ex_rs1;
+	ex_rd_tb       <= ex_rd;
+	
+	id_immed_tb    <= id_immed;
+	ex_immed_tb    <= ex_immed;
+	
+	id_rs3_ptr_tb  <= id_rs3_ptr;
+	id_rs2_ptr_tb  <= id_rs2_ptr;
+	id_rs1_ptr_tb  <= id_rs1_ptr;
+	id_rd_ptr_tb   <= id_rd_ptr;
+	
+	id_wback_tb    <= id_wback;
+	
+	ex_rs3_ptr_tb  <= ex_rs3_ptr;
+	ex_rs2_ptr_tb  <= ex_rs2_ptr;
+	ex_rs1_ptr_tb  <= ex_rs1_ptr;
+	ex_rd_ptr_tb   <= ex_rd_ptr;
+	
+	ex_wback_tb    <= ex_wback;
+	
+	wb_rd_tb       <= wb_rd;
+	wb_rd_ptr_tb   <= wb_rd_ptr;
+	wb_wback_tb    <= wb_wback;
+
+end architecture;
---
+++ src/Multimedia_Processor_Unit_VHDL/Multimedia_Processor_Unit_tb.vhd
@@ -0,0 +1,196 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use std.textio.all;
+use ieee.std_logic_textio.all;
+use ieee.numeric_std.all;
+
+use work.numeric_var.all;
+use work.all;
+
+entity Multimedia_Processor_Unit_tb is
+end Multimedia_Processor_Unit_tb;
+
+architecture test_bench of Multimedia_Processor_Unit_tb is
+	--unit input
+    signal clk         : std_logic := '0';
+    signal enable      : std_logic := '0';
+    signal reset_bar   : std_logic := '0';
+
+    signal in_file     : std_logic_vector(FILE_SIZE-1 downto 0) := (others => '0');	
+	
+	--unit output
+    signal out_file    : std_logic_vector(REGISTER_SIZE-1 downto 0) := (others => '0');
+	
+	--test beanch signals
+    signal pc_count    : std_logic_vector(COUNTER_LENGTH-1 downto 0) := (others => '0');
+    signal if_instruc  : std_logic_vector(INSTRUCTION_LENGTH-1 downto 0) := (others => '0');
+    signal id_instruc  : std_logic_vector(INSTRUCTION_LENGTH-1 downto 0) := (others => '0');
+
+    signal id_opcode   : std_logic_vector(OPCODE_LENGTH-1 downto 0) := (others => '0');
+    signal ex_opcode   : std_logic_vector(OPCODE_LENGTH-1 downto 0) := (others => '0');
+
+    signal id_rs3, id_rs2, id_rs1 : std_logic_vector(REGISTER_LENGTH-1 downto 0) := (others => '0');
+    signal ex_rs3, ex_rs2, ex_rs1 : std_logic_vector(REGISTER_LENGTH-1 downto 0) := (others => '0');
+    signal ex_rd                  : std_logic_vector(REGISTER_LENGTH-1 downto 0) := (others => '0');
+
+    signal id_immed  : std_logic_vector(IMMEDIATE_LENGTH-1 downto 0) := (others => '0');
+    signal ex_immed  : std_logic_vector(IMMEDIATE_LENGTH-1 downto 0) := (others => '0');
+
+    signal id_rs3_ptr, id_rs2_ptr, id_rs1_ptr, id_rd_ptr :
+           std_logic_vector(ADDRESS_LENGTH-1 downto 0) := (others => '0');
+
+    signal id_wback : std_logic := '0';
+
+    signal ex_rs3_ptr, ex_rs2_ptr, ex_rs1_ptr, ex_rd_ptr :
+           std_logic_vector(ADDRESS_LENGTH-1 downto 0) := (others => '0');
+
+    signal ex_wback  : std_logic := '0';
+    signal wb_rd     : std_logic_vector(REGISTER_LENGTH-1 downto 0) := (others => '0');
+    signal wb_rd_ptr : std_logic_vector(ADDRESS_LENGTH-1 downto 0) := (others => '0');
+    signal wb_wback  : std_logic := '0';
+
+    constant PERIOD : time := 10 ns;
+
+    function slv_to_str(v : std_logic_vector) return string is
+        variable s : string(1 to v'length);
+    begin
+        for i in v'range loop
+            case v(i) is
+                when '0' => s(v'length - i) := '0';
+                when '1' => s(v'length - i) := '1';
+                when others => s(v'length - i) := 'X';
+            end case;
+        end loop;
+        return s;
+    end function;
+
+begin
+
+    UUT : entity work.Multimedia_Processor_Unit
+        port map(
+            clk       => clk,
+            enable    => enable,
+            reset_bar => reset_bar,
+            in_file   => in_file, 
+			
+            out_file_tb => out_file,
+			
+            pc_count_tb   => pc_count,
+            if_instruc_tb => if_instruc,
+            id_instruc_tb => id_instruc,
+            id_opcode_tb  => id_opcode,
+            ex_opcode_tb  => ex_opcode,
+            id_rs3_tb     => id_rs3,
+            id_rs2_tb     => id_rs2,
+            id_rs1_tb     => id_rs1,
+            ex_rs3_tb     => ex_rs3,
+            ex_rs2_tb     => ex_rs2,
+            ex_rs1_tb     => ex_rs1,
+            ex_rd_tb      => ex_rd,
+            id_immed_tb   => id_immed,
+            ex_immed_tb   => ex_immed,
+            id_rs3_ptr_tb => id_rs3_ptr,
+            id_rs2_ptr_tb => id_rs2_ptr,
+            id_rs1_ptr_tb => id_rs1_ptr,
+            id_rd_ptr_tb  => id_rd_ptr,
+            id_wback_tb   => id_wback,
+            ex_rs3_ptr_tb => ex_rs3_ptr,
+            ex_rs2_ptr_tb => ex_rs2_ptr,
+            ex_rs1_ptr_tb => ex_rs1_ptr,
+            ex_rd_ptr_tb  => ex_rd_ptr,
+            ex_wback_tb   => ex_wback,
+            wb_rd_tb      => wb_rd,
+            wb_rd_ptr_tb  => wb_rd_ptr,
+            wb_wback_tb   => wb_wback
+        );	
+		
+	-- read from in_file + write to register_file.txt in little endian
+	Write_Back : process
+        file f_out : text;
+        variable L        : line;
+        variable word128  : std_logic_vector(127 downto 0);
+        variable reg4096  : std_logic_vector(REGISTER_SIZE-1 downto 0);
+        variable cycle_cnt: natural := 0;
+    begin
+        file_open(f_out, "src/register_file.txt", write_mode);
+        file_close(f_out);
+
+        wait until wb_wback = '1';
+        while true loop
+            wait for 1 ns;
+            reg4096 := out_file;
+
+            if wb_wback = '1' then 
+				
+                file_open(f_out, "src/register_file.txt", write_mode);
+
+                for i in 0 to 31 loop
+                    word128 := reg4096(
+                        ((i+1)*REGISTER_LENGTH)-1
+                        downto
+                        i*REGISTER_LENGTH
+                    );
+                    write(L, slv_to_str(word128));
+                    writeline(f_out, L);
+                end loop;
+
+                file_close(f_out);	  
+				wait until rising_edge(clk);
+            end if;
+        end loop;
+	end process;	  
+	
+	-- clk 
+	clk_process : process
+    begin
+        clk <= '0';  wait for PERIOD/2;
+        clk <= '1';  wait for PERIOD/2;
+    end process;
+	
+	-- enable and reset
+	inital : process
+    begin
+        enable    <= '0';
+        reset_bar <= '0';
+        wait for PERIOD/2;
+        reset_bar <= '1';
+        enable    <= '1';
+        wait;
+    end process;
+	
+	-- read from instruction_file.txt + write into in_file in little endian
+    Load_Instructions : process
+        file f_in  : text open read_mode is "instruction_file.txt";
+        variable L        : line;
+        variable str      : string(1 to INSTRUCTION_LENGTH);
+        variable chunk    : std_logic_vector(INSTRUCTION_LENGTH-1 downto 0);
+        variable little_endian : std_logic_vector(FILE_SIZE-1 downto 0);
+        variable index    : integer := 0;
+    begin
+        little_endian := (others => '0');
+
+        while not endfile(f_in) loop
+            readline(f_in, L);
+            read(L, str);
+
+            for i in 1 to INSTRUCTION_LENGTH loop
+                if str(i) = '0' then
+                    chunk(INSTRUCTION_LENGTH - i) := '0';
+                elsif str(i) = '1' then
+                    chunk(INSTRUCTION_LENGTH - i) := '1';
+                else
+                    chunk(INSTRUCTION_LENGTH - i) := 'X';
+                end if;
+            end loop;
+
+            little_endian(index + INSTRUCTION_LENGTH - 1 downto index) := chunk;
+            index := index + INSTRUCTION_LENGTH;
+        end loop;
+
+        in_file <= little_endian;
+        wait;
+    end process;
+
+end architecture;
+
+
